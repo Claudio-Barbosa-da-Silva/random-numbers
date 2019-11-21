6c0acf623efac4eaa0fe935f858f96b03aa32663
Claudio Barbosa <claudiobarbosa@hotmail.com>
2019-11-20 22:27:38 -0300

    first commit

---
+++ src/main/java/ImplementarAdapter/RandomAdapter.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ImplementarAdapter;
+
+import org.apache.commons.math3.distribution.LogNormalDistribution;
+import org.apache.commons.math3.distribution.RealDistribution;
+
+/**
+ *
+ * @author aluno
+ */
+public class RandomAdapter implements Gerador{
+
+    @Override
+    public int[] getGerador(int sampleSize) {
+        RealDistribution exponentialRandom = new LogNormalDistribution();
+        long seed = 0;
+        exponentialRandom.reseedRandomGenerator(seed);
+        return null;
+        
+    
+    }
+    
+    
+  
+    
+}
